33afa3b1b587196620c1de3484bec0510e34bbf6
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-04-02 18:50:07 +0300

    Order complete admin panel added

--- Store/StoreApp/Areas/Admin/Controllers/CategoryController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/CategoryController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace StoreApp.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area(nameof(Admin))]
     public class CategoryController : Controller
     {
         public IActionResult Index()
---
+++ Store/StoreApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    public class OrderController : Controller
+    {
+        private readonly IServiceManager _manager;
+
+        public OrderController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = _manager.OrderService.Orders;
+            return View(orders);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Complete(int id)
+        {
+            await _manager.OrderService.Complete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
