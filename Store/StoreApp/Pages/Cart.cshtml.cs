7626b2f738f70113714c213f7cb1765d7a3e3338
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-04-02 17:00:51 +0300

    HttpRequestExtension for redirecting user to page who came from.

--- Store/StoreApp/Pages/Cart.cshtml.cs
+++ Store/StoreApp/Pages/Cart.cshtml.cs
@@ -29,7 +29,7 @@ namespace StoreApp.Pages
             {
                 Cart.AddItem(product, 1);
             }
-            return Page(); // returnUrl
+            return RedirectToPage(new {returnUrl=returnUrl}); // returnUrl
         }
 
         public IActionResult OnPostRemove(int id, string returnUrl)
